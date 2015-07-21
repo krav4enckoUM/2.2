f920158576b823dcaa1474c102792a91d2070771
krav4enckoUM <iurakrav4encko@yandex.ru>
2015-07-21 22:11:27 +0500

    код

---
+++ Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    class Word
+    {
+        private string wordValue; //Значение слова
+        private int numberOfDifferentLetters = 0; //Кол-во разных букв в слове
+
+        public Word(string n) //Конструктор
+        {
+            if (n.Length > 0) //Определяем само слово
+            {
+                wordValue = n;
+            }
+            else wordValue = "DefaultWord";
+            numberOfDifferentLetters = NumberOfDifferentLetters; //Определяем кол-во разных букв
+        }
+
+        public string WordValue
+        {
+            get { return wordValue; }
+            set { wordValue = value; }
+        } //Свойство доступа к переменной wordValue
+
+        public int NumberOfDifferentLetters
+        {
+            get
+            {
+                var groups = WordValue.ToCharArray().GroupBy(c => c); //Лямбда-выражение для поиска разных букв
+                foreach (var char_group in groups)
+                    numberOfDifferentLetters++; //Подсчет кол-ва разных букв
+                return numberOfDifferentLetters;
+            }
+        }//Свойство доступа к переменной numberOfDifferentLetters - Read Only
+
+        public void OnScreen() //Метод вывода информации на экран
+        {
+            Console.Write("{0}: Длина слова: {1}, Кол-во разных букв: {2}.\n", wordValue, wordValue.Length, numberOfDifferentLetters);
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    class Program
+    {
+        static void WriteList(List<Word> wordList) //Функция, выводящая весь список на экран
+        {
+            foreach (Word word in wordList)
+            {
+                word.OnScreen();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Random rnd = new Random(); //Подключаем ГСЧ
+            List<Word> WordList = new List<Word> //Создаем список
+            {
+                new Word("Hello") {},
+                new Word("World") {},
+                new Word("This") {},
+                new Word("Is") {},
+                new Word("Word") {}, //Заполняем экземплярами класса Word
+            };
+
+            Console.WriteLine("Список:\n");
+            WriteList(WordList);
+
+            Console.WriteLine("\nСортировка по длине слова:\n");
+            WriteList(WordList.OrderBy(l => l.WordValue.Length).ToList());
+
+            Console.WriteLine("\nДобавление элемента в конец:\n");
+            WordList.Add(new Word("Qwerty") { });
+            WriteList(WordList);
+
+            Console.WriteLine("\nДобавление элемента после 3 позиции:\n");
+            WordList.Insert(3, new Word("Test") { });
+            WriteList(WordList);
+
+            Console.WriteLine("\nУдаление обоих элементов:\n");
+            WordList.RemoveAt(3);
+            WordList.RemoveAt(5);
+            WriteList(WordList);
+
+            Console.WriteLine("\nПоиск по имени \"This\":\n");
+            int i = WordList.FindIndex(k => k.WordValue == "This");
+            Console.WriteLine("Результаты поиска:\n");
+            WordList[i].OnScreen();
+
+            Console.WriteLine("\nОчищенный список:\n");
+            WordList.Clear();
+            WriteList(WordList);
+
+            Console.ReadKey();
+        }
+    }
+}
+
